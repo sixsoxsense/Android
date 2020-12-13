2f1769598a043fd22557d17d6d23c3ac9fdce5b8
dudwn <dudwn2523@gmail.com>
2020-12-13 23:25:53 +0900

    update

---
+++ subject/src/main/java/com/example/subject/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.subject;
+
+import android.content.Intent;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+}
